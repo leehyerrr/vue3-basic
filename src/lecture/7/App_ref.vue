0b24eba461ad64c265e6e95f4aed73f17e5b7542
leehyerrr <leehyerrr@gmail.com>
2026-07-13 22:34:07 +0900

    메인에서 수정

--- src/lecture/7/App_ref.vue
+++ src/lecture/7/App_ref.vue
@@ -21,7 +21,7 @@ export default {
 
     const styleObject = reactive({
       color: 'red',
-      fontSize: '13px',
+      fontSize: '20px', //다시 메인에서 수정
     })
 
     return { message, addMessage, reactiveMessage, addMessage2, styleObject }
