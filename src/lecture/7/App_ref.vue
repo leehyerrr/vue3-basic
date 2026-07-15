f2f050b79091f0768aa24c8d6a7fbeab69a26c22
leehyerrr <102867392+leehyerrr@users.noreply.github.com>
2026-07-16 06:44:54 +0900

    ㅇㄹㅇㅇ

--- src/lecture/7/App_ref.vue
+++ src/lecture/7/App_ref.vue
@@ -22,7 +22,7 @@ export default {
     const styleObject = reactive({
       color: 'red',
 
-      fontSize: '17px', //원격 수정2
+      fontSize: '17px', //원격 수정3
     })
 
     return { message, addMessage, reactiveMessage, addMessage2, styleObject }
