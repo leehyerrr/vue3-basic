acb356dda9e363fbf5efe1c654eb7949efc7a704
leehyerrr <leehyerrr@gmail.com>
2026-07-16 07:02:54 +0900

    xx

--- src/lecture/7/App_ref.vue
+++ src/lecture/7/App_ref.vue
@@ -22,7 +22,7 @@ export default {
     const styleObject = reactive({
       color: 'red',
 
-      fontSize: '10px', //test 수정2
+      fontSize: '10px', //test 수정3
     })
 
     return { message, addMessage, reactiveMessage, addMessage2, styleObject }
