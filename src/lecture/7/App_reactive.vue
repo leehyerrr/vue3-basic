8a1c1f75dd779baa0bd9dd83e461fc28b6f0944c
leehyerrr <102867392+leehyerrr@users.noreply.github.com>
2026-07-19 22:52:48 +0900

    dd

--- src/lecture/7/App_reactive.vue
+++ src/lecture/7/App_reactive.vue
@@ -13,7 +13,7 @@ export default {
     const state = reactive({ count: 0, deep: { count: 0 } })
     const increament = () => {
       state.count++
-      console.log('cc')
+      console.log('dd')
       state.deep.count++
     }
     return { state, increament }
